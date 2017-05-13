067eb3e1e8a8aa1b6ceb35127d6c3d4fb270d598
Sage911 <stefan.age@outlook.com>
2017-05-12 19:47:18 -0500

    Generating Controllers - providers, jobs, contacts, users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+
+  # actions serve to collect information and provide the controller with a view
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources:providers
+  resources:jobs
+  resources:contacts
+  resources:users
+  #resourceful route declares routes for index, show, new, edit, create, update and destroy actions
+
 end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
