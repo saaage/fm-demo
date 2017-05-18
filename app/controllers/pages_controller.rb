107c5be6f314ab1bc7f8dee3cbfd2a65e21ed837
Sage911 <stefan.age@outlook.com>
2017-05-18 11:36:14 -0500

    Setting up search and login pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def welcome
   end
+
+  def search
+  end
+
+  def login
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  root 'pages#welcome'
+  root 'pages#login'
 
   resources :providers, :jobs, :contacts, :clients, :users
   #resourceful route declares routes for index, show, new, edit, create, update and destroy actions
 
   get 'register', to: 'providers#new'
   # allows us to use a single resource to map /register to provider#new action
+
+  get 'search', to: 'pages#search'
 
 end
 
