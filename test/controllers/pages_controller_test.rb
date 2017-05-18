42ac39639fe360a40e95a9a94e83fb41fbd3b79c
Sage911 <stefan.age@outlook.com>
2017-05-18 13:49:31 -0500

    Started TDD of Pages controller

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get login" do
+    get '/'
+    assert_response :success
+  end
+
+  test "should get register" do
+    get '/register'
+    assert_response :success
+  end
+
+  test "should get search" do
+    get '/search'
+    assert_response :success
+  end
+
 end
