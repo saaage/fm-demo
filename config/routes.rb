f0d440e0aa54cc11d22390888d4b3d2f64d16795
Sage911 <stefan.age@outlook.com>
2017-05-22 17:05:24 -0500

    Added Sign Up link to login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
   root 'pages#login'
 
   resources :providers, :jobs, :contacts, :clients, :users
   #resourceful route declares routes for index, show, new, edit, create, update and destroy actions
 
-  get 'register', to: 'providers#new'
-  # allows us to use a single resource to map /register to provider#new action
   get 'search', to: 'pages#search'
+  get 'signup', to: 'users#new'
   get 'dashboard', to: 'pages#home'
 
 end
