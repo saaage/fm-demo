191377b1ba163c30727788533c70e0ed14ef1ec4
Sage911 <stefan.age@outlook.com>
2017-05-25 12:47:45 -0500

    Stubbed user profile page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  # Return Gravatar for the given user
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    # this final line is the return value of gravatar_for (an image tag)
+  end
+
 end
