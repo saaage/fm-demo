3213ba52c3496d6929cc059065298e1a002a10b4
Sage911 <stefan.age@outlook.com>
2017-06-06 17:21:57 -0500

    Users must be logged in to edit or update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update]
+  # applies :logged_in_user to 'edit' and 'update'
+
   def show
     @user = User.find(params[:id])
   end
@@ -39,6 +42,14 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+      #utilizing logged_in? from our sessions helper
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 
 end
