de903eace099db4933d1d8a2bd1f1129d56a4508
Sage911 <stefan.age@outlook.com>
2017-06-20 17:49:53 -0500

    Our first tests for Contact model

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @contact_via_client_contact = contacts(:malory)
+    @contact_via_non_client_contact = contacts(:archer)
+  end
+
+  test "Contact can be created using role type client_admin" do
+    assert @contact_via_client_contact.valid?
+  end
+
+  test "Contact can not be created with using a non client_admin role types" do
+    assert_not @contact_via_non_client_contact.valid?
+  end
+
 end
