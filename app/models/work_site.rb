4e99169abb665cf3fe67669463f01f547ed16031
Sage911 <stefan.age@outlook.com>
2017-06-21 13:35:25 -0500

    Add/Test validations for Work Site model

--- app/models/work_site.rb
+++ app/models/work_site.rb
@@ -2,4 +2,6 @@ class WorkSite < ApplicationRecord
   belongs_to :client
   belongs_to :state
   # has_and_belongs_to_many :jobs
+  validates :address, presence: { message: "Please enter an address" }, length: { maximum: 50, message: "Address is too long" }, uniqueness: { message: "Work site already exists" }
+
 end
--- test/models/work_site_test.rb
+++ test/models/work_site_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class WorkSiteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @worksite = work_sites(:dr_bob_ent)
+  end
+
+  test "Work Sites can be created" do
+    assert  @worksite.valid?
+  end
+
+  test "Address can not be blank" do
+    @worksite_blank_address = WorkSite.new( client_id: 1,
+                              address: "",
+                              state_id: 1)
+    assert_not @worksite_blank_address.save
+  end
+
 end
